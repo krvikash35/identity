730537fe76d77f57240f00f28ecb396ef145359a
Agrawal <Yogesh.Agrawal@unilever.com>
2017-05-31 18:39:32 +0530

    updated

--- index.js
+++ index.js
@@ -2,64 +2,32 @@ const http = require('http');
 const path = require('path');
 
 const express = require('express');
+console.log("nodepath", process.env.NODE_PATH)
 var logger = require('logging/logger');
 
-var obj = {
-    name: "vikash",
-    address: {
-        city: "kolkata"
-    }
-}
-logger.info("hello", obj)
-logger.error("error")
-// console.log("environment: ", process.env.NODE_PATH)
 
-//import module and initialize if necessary ....logging_mod, db_mod, http_module, routing module 
+const app = express();
+app.listen(8082, ()=>{
+  console.log("listening on 8082");
+})
 
-
-
-
-
-// const db = require('services/db.service');
-// const config = require('config/config');
-
-// const app = express();
-// app.listen(8082, ()=>{
-//   console.log("listening on 8082");
-// })
+logger.info("Hi")
+app.get("/service", (req, res)=>{
+    logger.info({req: req})
+    // logger.info(req)
+    res.status(200).send("HI")
+    logger.info({res: res})
+})
 
 
-// app.use()
 
 
-// app.use(function(req, res, next) {
-//   // res.header("Access-Control-Allow-Origin", "*");
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-// app.use('/public', express.static(path.join(__dirname, 'public')));
-// app.get("/login", (req, res)=>{
-//   res.sendFile( path.join( __dirname,  "public/html/login.html") )
-  
-// })
 
-
-// app.get("/loginwithgoogle", (req, res)=>{
-//   res.redirect(302, "http://google.com");
-// })
-
-
-// const gauth = require('services/auth/auth.google.service')
-
-// async function demo(){
-
-
-// try{
-//   let data = await gauth.getDiscDoc(config.auth.openid_google.discovery_endpoint);
-//   console.log("data", data);
-// }catch(e){
-//   console.log("error", e)
+// var me = {
+//     login: 'krvikash35',
+//     name: 'vikash kumar'
 // }
-
-// }
-// demo()
+// var err = new Error("sample error");
+// logger.error(me)
+// // logger.error({err: me}, "error occured")
+// logger.info( {user: me}, 'logged in' )
--- logging/logger.js
+++ logging/logger.js
@@ -1,21 +1,48 @@
 var bunyan = require('bunyan');
 var log;
 
-if (process.env.NODE_ENV = "PROD") {
+if (process.env.NODE_ENV === "PROD") {
+    console.log("prod logger")
     log = bunyan.createLogger({
         name: 'myapp',
         streams: [{
-            stream: process.stderr
-            // `type: 'stream'` is implied
+            name: 'prod_std_stream',
+            type: 'stream',
+            stream: process.stdout,
+            level: bunyan.WARN,
+            serializers: bunyan.stdSerializers
         },
         {
+            name: 'prod_file_stream',
             type: 'file',
             path: 'identity.log',
-            level: bunyan.ERROR
+            level: bunyan.ERROR,
+            serializers: bunyan.stdSerializers
         }]
     });
 } else {
-
+    console.log("dev logger")
+    log = bunyan.createLogger({
+        name: 'myapp',
+        memory: new Date(),
+        serializers: {
+            err: bunyan.stdSerializers.err,
+            req: bunyan.stdSerializers.req,
+            res: bunyan.stdSerializers.res    
+        },
+        streams: [{
+            name: 'dev_std_stream',
+            type: 'stream',
+            stream: process.stdout,
+            level: bunyan.DEBUG
+        },
+        {
+            name: 'dev_file_stream',
+            type: 'file',
+            path: 'identity.log',
+            level: bunyan.INFO
+        }]
+    });
 }
 
 module.exports = log;
